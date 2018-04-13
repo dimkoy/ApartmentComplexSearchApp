fe4d28640b63b991a79369d66f942338683c9d5a
dimkoy <dimkoy01@gmail.com>
2018-04-13 21:25:05 +0400

    Created project

---
+++ ApartmentComplexSearchApp/ApartmentComplexSearchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApartmentComplexSearchApp
+//
+//  Created by Dmitriy on 13/04/2018.
+//  Copyright © 2018 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApartmentComplexSearchApp/ApartmentComplexSearchApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApartmentComplexSearchApp
+//
+//  Created by Dmitriy on 13/04/2018.
+//  Copyright © 2018 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
